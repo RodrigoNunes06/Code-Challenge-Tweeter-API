821b67b809c7ff056efd7ff09016a8355fa168a8
Rodrigo Nunes <rodrigo.nunes06@gmail.com>
2016-10-10 11:28:45 +0200

    Create project, resources User, model Tweets

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+User.create( name: "Rodrigo", email:"rodrigo.nunes06@gmail.com", password: "12345")
+User.create( name: "Juan", email:"juan@example.com", password: "12345")
+User.create( name: "Pepe", email:"pepe@example.com", password: "12345")
+User.create( name: "Maria", email:"maria@example.com", password: "12345")
